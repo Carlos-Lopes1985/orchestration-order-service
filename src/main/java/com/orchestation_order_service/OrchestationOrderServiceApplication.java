0005a094763c36b2d01e42ba8ec148a6b788dec7
Carlos-Lopes1985 <carlosslopes1985@hotmail.com>
2025-04-05 15:11:56 -0300

    primeiro commit- configuração projeto

---
+++ src/main/java/com/orchestation_order_service/OrchestationOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.orchestation_order_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrchestationOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrchestationOrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orchestation_order_service/OrchestationOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orchestation_order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchestationOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
